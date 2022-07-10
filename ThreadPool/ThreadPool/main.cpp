4df738f9e0482b664b257d911f724aae46a10d58
setary <shuhui_zh@163.com>
2022-07-10 17:50:23 +0800

    realize a simple ThreadPool

---
+++ ThreadPool/ThreadPool/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ThreadPool
+//
+//  Created by 张淑慧 on 2022/7/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
